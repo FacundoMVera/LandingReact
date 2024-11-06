1caf9052c6da4225307fea24c98b9778d462cb37
Facundo Vera <facu8841@gmail.com>
2024-11-05 22:59:59 -0300

    Proyecto Final Molina Vera

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { CartProvider } from './Context/CartContext'; 
-import './app.css'; 
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
